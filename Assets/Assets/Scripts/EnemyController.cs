e6e5f468f7b2519bb6eda12fb339010666529395
bince25 <incebaran25@gmail.com>
2022-09-03 16:05:15 +0300

    gamemanager

--- Assets/Assets/Scripts/EnemyController.cs
+++ Assets/Assets/Scripts/EnemyController.cs
@@ -5,48 +5,60 @@ using UnityEngine;
 public class EnemyController : CharacterBaseClass
 {
     private PlayerController playerController;
+    private GameManager gameManager;
+    private void Start()
+    {
+        currentHealth = fullHealth;   
+    }
     private void Update()
     {
-        if (playerController.healthPercentage < this.healthPercentage)
+        if (gameManager.turnNumber == 1)
         {
-            //agresif
-            int dice100 = UnityEngine.Random.Range(1, 101);
-            if (dice100 <= 60)
+            if (playerController.healthPercentage < this.healthPercentage)
             {
-                //Attack
+                //agresif
+                int dice100 = UnityEngine.Random.Range(1, 101);
+                if (dice100 <= 60)
+                {
+                    //Attack
+                    playerController.getDamage(strength);
+                }
+                else if (60 < dice100 && dice100 <= 80)
+                {
+                    changeShield(UnityEngine.Random.Range(1, 5)); // 5 rastgele yazýldý
+                }
+                else if (80 < dice100 && dice100 <= 90)
+                {
+                    //Sleep
+                }
+                else if (90 < dice100 && dice100 <= 100)
+                {
+                    //Buff
+                }
             }
-            else if (60 < dice100 && dice100 <= 80)
+            else if (playerController.healthPercentage > this.healthPercentage)
             {
-                //Guard
-            }
-            else if (80 < dice100 && dice100 <= 90)
-            {
-                //Sleep
-            }
-            else if (90 < dice100 && dice100 <= 100)
-            {
-                //Buff
-            }
-        }
-        else if (playerController.healthPercentage > this.healthPercentage)
-        {
-            int dice100 = UnityEngine.Random.Range(1, 101);
-            if (dice100 <= 60)
-            {
-                //Guard
-            }
-            else if (60 < dice100 && dice100 <= 80)
-            {
-                //Attack
-            }
-            else if (80 < dice100 && dice100 <= 90)
-            {
-                //Sleep
-            }
-            else if (90 < dice100 && dice100 <= 100)
-            {
-                //Buff
-            }
+                //defansif
+                int dice100 = UnityEngine.Random.Range(1, 101);
+                if (dice100 <= 60)
+                {
+                    //Guard
+                    changeShield(UnityEngine.Random.Range(1, 5)); // 5 rastgele yazýldý
+                }
+                else if (60 < dice100 && dice100 <= 80)
+                {
+                    //Attack
+                    playerController.getDamage(strength);
+                }
+                else if (80 < dice100 && dice100 <= 90)
+                {
+                    //Sleep
+                }
+                else if (90 < dice100 && dice100 <= 100)
+                {
+                    //Buff
+                }
+            }       
         }   
     }
     public void getDamage(float damage)
@@ -68,5 +80,6 @@ public class EnemyController : CharacterBaseClass
     public void die()
     {
         //die
+        Destroy(this.gameObject);
     }
 }
---
+++ Assets/Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //Turn System
+    public int turnNumber = 0; // 0 --> Player, 1 --> Enemy
+
+    void Update()
+    {
+        
+    }
+}
