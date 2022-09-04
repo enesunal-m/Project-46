d88be5a498b1fc15166834be3ea93828c50e12b4
bince25 <incebaran25@gmail.com>
2022-09-04 04:11:18 +0300

    CardFinish
    Id alınacak

--- Assets/Assets/Scripts/CardMouseInteraction.cs
+++ Assets/Assets/Scripts/CardMouseInteraction.cs
@@ -7,28 +7,21 @@ using UnityEngine.EventSystems;
 
 public class CardMouseInteraction : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
-    Vector3 defaultPos;
     GameObject selectedGameObject;
     private void Start()
     {
         this.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
-        
     }
     public void OnPointerEnter(PointerEventData eventData)
     {
-        defaultPos = this.transform.position;
-        this.transform.position = new Vector2(this.GetComponent<RectTransform>().position.x, this.GetComponent<RectTransform>().position.y + 150);
-        //this.GetComponent<RectTransform>().anchorMax = new Vector2(0, 1.5f);
-        //this.GetComponent<RectTransform>().anchorMin = new Vector2(0, 1.5f);
-        //this.GetComponent<RectTransform>().localScale = new Vector3(1.2f, 1.2f, 1);
+        this.GetComponent<RectTransform>().localScale = new Vector3(1.2f, 1.2f, 1);
+        this.GetComponent<Canvas>().sortingOrder = 1;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        this.transform.position = defaultPos;
-        //this.GetComponent<RectTransform>().anchorMax = new Vector2(0, 1);
-        //this.GetComponent<RectTransform>().anchorMin = new Vector2(0, 1);
-        //this.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+        this.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+        this.GetComponent<Canvas>().sortingOrder = 0;
     }
 
     public void OnPointerClick(PointerEventData eventData)
