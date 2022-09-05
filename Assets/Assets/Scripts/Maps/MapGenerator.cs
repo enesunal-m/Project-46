d2bc648fab8039be135e1fd2d99f2f03277f3d94
berketurker <112757322+berketurker@users.noreply.github.com>
2022-09-05 16:35:48 +0300

    Nodes Created

--- Assets/Assets/Scripts/Maps/MapGenerator.cs
+++ Assets/Assets/Scripts/Maps/MapGenerator.cs
@@ -1,166 +1,166 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
 
-public class NodeCollector
-{   //Created this class to define floor and positions for nodes
-    public int floorCount;
-    public Vector3 newNodePosition;
-}
+//public class NodeCollector
+//{   //Created this class to define floor and positions for nodes
+//    public int floorCount;
+//    public Vector3 newNodePosition;
+//}
 
-public class MapGenerator : MonoBehaviour
-{
-    [SerializeField] GameObject node, canvas, mapImageToSlide;
-    //map, lineSprite yukarý taþý
-    [HideInInspector] public int totalNodesBeforeBossFight, floorCount, floorSpawnCounter, destroyedCounter;
-    [HideInInspector] public int[] floorCountAndIndexOfNode;
-    [HideInInspector] public Vector3 newNodePosition;
-    
-    
-    float nodeDiameter, portionalWidthOfMap, portionalHeightOfMap;
-    
-    int[] chances;
-    int seed, counter;
-    NodeCollector collector;
-    List<NodeCollector> floorAndNodePosition = new List<NodeCollector>();
-
-    private void Awake()
-    {
-
-        NodeCreation();
-        HideNode();
-    }
-
-    void Start()
-    {
+//public class MapGenerator : MonoBehaviour
+//{
+//    [SerializeField] GameObject node, canvas, mapImageToSlide;
+//    //map, lineSprite yukarý taþý
+//    [HideInInspector] public int totalNodesBeforeBossFight, floorCount, floorSpawnCounter, destroyedCounter;
+//    [HideInInspector] public int[] floorCountAndIndexOfNode;
+//    [HideInInspector] public Vector3 newNodePosition;
 
 
-    }
+//    float nodeDiameter, portionalWidthOfMap, portionalHeightOfMap;
 
-    void LineDraw()
-    {
+//    int[] chances;
+//    int seed, counter;
+//    NodeCollector collector;
+//    List<NodeCollector> floorAndNodePosition = new List<NodeCollector>();
 
-    }
+//    private void Awake()
+//    {
 
-    void NodeClassification()
-    {
+//        NodeCreation();
+//        HideNode();
+//    }
 
-    }
-
-
-
-    void HideNode()
-    {
-        int rand;
-        float portionalWidthOfMap = canvas.GetComponent<CanvasScaler>().referenceResolution.x; //Took the dimensions of the map
-        float portionalHeightOfMap = canvas.GetComponent<CanvasScaler>().referenceResolution.y;
-        float nodeDiameter = node.GetComponent<RectTransform>().rect.width;//Could've used height
-        float k = Random.Range(-portionalWidthOfMap * 0.05f, portionalHeightOfMap * 0.05f);//Randomly positioning in a radii
-        destroyedCounter = 0;
+//    void Start()
+//    {
 
 
-        node.name = "node" + i + "x" + j;
-        
-        newNodePosition = new Vector3(k + nodeDiameter * 2 - portionalWidthOfMap / 2 + i * portionalWidthOfMap / 7, k + nodeDiameter - portionalHeightOfMap / 11 + portionalHeightOfMap / 3 * j, -1);
-        var Image = Instantiate(node, newNodePosition, Quaternion.identity); //totalNodesBeforeBossFight will be added as divider of portional height of map
-        Image.transform.SetParent(mapImageToSlide.transform, false);
-        counter++;
+//    }
 
-        {
-            for (int floor = 0; floor < 10; floor++)
-            {
-                seed = Random.Range(0, 50);
-                if (seed <= 5)//%10 chance
-                    for (int row = 0; row < 3; row++)//Create 4 random objects
-                    {
-                        rand = Random.Range(0, 6);
-                        
-                    }
+//    void LineDraw()
+//    {
 
-                else if (seed <= 20)
-                {
-                    for (int i = 0; i < 4; i++)
-                    {
-                        rand = Random.Range(0, 6);
-                        GameObject go = GameObject.Find("node" + floor + "x" + rand + "(Clone)");
-                        if (go)
-                        {
-                            Destroy(go.gameObject);
-                        }
-                        else
-                        {
-                            print("Nesne yok. " + floor + ". kat" + rand + ". obje");
-                        }
+//    }
 
-                    }
-                    //Destroy 4 random objects
-                }
-                else
-                {
-                    for (int i = 0; i < 5; i++)
-                    {
-                        rand = Random.Range(0, 6);
-                        GameObject go = GameObject.Find("node" + floor + "x" + rand + "(Clone)");
-                        if (go)
-                        {
-                            Destroy(go.gameObject);
-                        }
-                        else
-                        {
-                            print("Nesne yok. " + floor + ". kat" + rand + ". obje");
-                        }
+//    void NodeClassification()
+//    {
 
-                    }
-                }
-            }
-            
-            
-        }
-
-        
-
-    }
-
-    /// <summary>
-    /// Spawn nodes.
-    /// </summary>
-    void NodeCreation()
-    {   
-        float portionalWidthOfMap = canvas.GetComponent<CanvasScaler>().referenceResolution.x; //Took the dimensions of the map
-        float portionalHeightOfMap = canvas.GetComponent<CanvasScaler>().referenceResolution.y;
-        float nodeDiameter = node.GetComponent<RectTransform>().rect.width;//Could've used height
-
-        for (int j = 0; j < 10; j++)
-        {
-
-            counter = 0;
-            floorCount++;//where we start to play is floor zero,first fight's at floor one.
-            for (var i = 0; i < 7; i++)
-            {
-
-                node.name = "node" + i + "x" + j;
-                float k = Random.Range(-portionalWidthOfMap*0.05f, portionalHeightOfMap * 0.05f);//Randomly positioning in a radii
-                newNodePosition = new Vector3(k + nodeDiameter * 2 - portionalWidthOfMap / 2 + i * portionalWidthOfMap / 7, k + nodeDiameter - portionalHeightOfMap / 2 + portionalHeightOfMap / 3 * j, -1);
-                var Image = Instantiate(node, newNodePosition, Quaternion.identity); //totalNodesBeforeBossFight will be added as divider of portional height of map
-                Image.transform.SetParent(mapImageToSlide.transform, false);
-                counter++;
-
-
-                
-                collector = new NodeCollector();
-                collector.floorCount = floorCount;
-                collector.newNodePosition = newNodePosition;
-                floorAndNodePosition.Add(collector);
-
-            }
-        }
+//    }
 
 
 
-    }
+//    void HideNode()
+//    {
+//        int rand;
+//        float portionalWidthOfMap = canvas.GetComponent<CanvasScaler>().referenceResolution.x; //Took the dimensions of the map
+//        float portionalHeightOfMap = canvas.GetComponent<CanvasScaler>().referenceResolution.y;
+//        float nodeDiameter = node.GetComponent<RectTransform>().rect.width;//Could've used height
+//        float k = Random.Range(-portionalWidthOfMap * 0.05f, portionalHeightOfMap * 0.05f);//Randomly positioning in a radii
+//        destroyedCounter = 0;
 
 
-}
+//        node.name = "node" + i + "x" + j;
+
+//        newNodePosition = new Vector3(k + nodeDiameter * 2 - portionalWidthOfMap / 2 + i * portionalWidthOfMap / 7, k + nodeDiameter - portionalHeightOfMap / 11 + portionalHeightOfMap / 3 * j, -1);
+//        var Image = Instantiate(node, newNodePosition, Quaternion.identity); //totalNodesBeforeBossFight will be added as divider of portional height of map
+//        Image.transform.SetParent(mapImageToSlide.transform, false);
+//        counter++;
+
+//        {
+//            for (int floor = 0; floor < 10; floor++)
+//            {
+//                seed = Random.Range(0, 50);
+//                if (seed <= 5)//%10 chance
+//                    for (int row = 0; row < 3; row++)//Create 4 random objects
+//                    {
+//                        rand = Random.Range(0, 6);
+
+//                    }
+
+//                else if (seed <= 20)
+//                {
+//                    for (int i = 0; i < 4; i++)
+//                    {
+//                        rand = Random.Range(0, 6);
+//                        GameObject go = GameObject.Find("node" + floor + "x" + rand + "(Clone)");
+//                        if (go)
+//                        {
+//                            Destroy(go.gameObject);
+//                        }
+//                        else
+//                        {
+//                            print("Nesne yok. " + floor + ". kat" + rand + ". obje");
+//                        }
+
+//                    }
+//                    //Destroy 4 random objects
+//                }
+//                else
+//                {
+//                    for (int i = 0; i < 5; i++)
+//                    {
+//                        rand = Random.Range(0, 6);
+//                        GameObject go = GameObject.Find("node" + floor + "x" + rand + "(Clone)");
+//                        if (go)
+//                        {
+//                            Destroy(go.gameObject);
+//                        }
+//                        else
+//                        {
+//                            print("Nesne yok. " + floor + ". kat" + rand + ". obje");
+//                        }
+
+//                    }
+//                }
+//            }
+
+
+//        }
+
+
+
+//    }
+
+//    /// <summary>
+//    /// Spawn nodes.
+//    /// </summary>
+//    void NodeCreation()
+//    {
+//        float portionalWidthOfMap = canvas.GetComponent<CanvasScaler>().referenceResolution.x; //Took the dimensions of the map
+//        float portionalHeightOfMap = canvas.GetComponent<CanvasScaler>().referenceResolution.y;
+//        float nodeDiameter = node.GetComponent<RectTransform>().rect.width;//Could've used height
+
+//        for (int j = 0; j < 10; j++)
+//        {
+
+//            counter = 0;
+//            floorCount++;//where we start to play is floor zero,first fight's at floor one.
+//            for (var i = 0; i < 7; i++)
+//            {
+
+//                node.name = "node" + i + "x" + j;
+//                float k = Random.Range(-portionalWidthOfMap * 0.05f, portionalHeightOfMap * 0.05f);//Randomly positioning in a radii
+//                newNodePosition = new Vector3(k + nodeDiameter * 2 - portionalWidthOfMap / 2 + i * portionalWidthOfMap / 7, k + nodeDiameter - portionalHeightOfMap / 2 + portionalHeightOfMap / 3 * j, -1);
+//                var Image = Instantiate(node, newNodePosition, Quaternion.identity); //totalNodesBeforeBossFight will be added as divider of portional height of map
+//                Image.transform.SetParent(mapImageToSlide.transform, false);
+//                counter++;
+
+
+
+//                collector = new NodeCollector();
+//                collector.floorCount = floorCount;
+//                collector.newNodePosition = newNodePosition;
+//                floorAndNodePosition.Add(collector);
+
+//            }
+//        }
+
+
+
+//    }
+
+
+//}
 
 
--- Assets/Assets/Scripts/Maps/MapGeneratorOop.cs
+++ Assets/Assets/Scripts/Maps/MapGeneratorOop.cs
@@ -5,26 +5,53 @@ using UnityEngine.UI;
 
 public class MapGeneratorOop : MonoBehaviour
 {
-        int row = 7;
-        int column = 10;
-        
-    // Start is called before the first frame update
+    int row = 7;
+    int column = 10;
+    [SerializeField] GameObject node, mapParent, canvas;
+
+    private void Awake()
+    {
+        //Canvas Dimensions
+
+        float portionalWidthOfMap = canvas.GetComponent<CanvasScaler>().referenceResolution.x;
+        float portionalHeightOfMap = canvas.GetComponent<CanvasScaler>().referenceResolution.y;
+        float nodeDiameter = node.GetComponent<RectTransform>().rect.width;
+        float k = Random.Range(-portionalWidthOfMap * 0.05f, portionalHeightOfMap * 0.05f);
+
+        //Canvas Dimensions
+    }
+
+
+    
     void Start()
     {
-        Button[,] buttons = new Button[column, row];
-        for (int i = 0; i < buttons.GetUpperBound(0); i++)
+
+        GameObject[,] nodes = new GameObject[column, row];
+        for (int i = 0; i < column; i++)
         {
-            for (int j = 0; j < buttons.GetUpperBound(1) j++)
+            for (int j = 0; j < row; j++)
             {
-                buttons[i, j] = new Button;
+
+                nodes[i, j] = Instantiate(node);
+                nodes[i, j].transform.SetParent(mapParent.transform, false);
+                nodes[i, j].name = "node" + i + "x" + j;
+
             }
 
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
 
+
+    Vector2 NodePositionerAndPositionHolder(GameObject[,] nodes)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                nodes[i, j].GetComponent<RectTransform>().position = Vector2.zero;//screen sizes
+            }
+        }
+        return Vector2.zero;
     }
 }
