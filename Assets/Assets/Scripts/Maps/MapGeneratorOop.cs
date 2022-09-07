1f9891ede238386dd2e0906d29557fbd44fd1dba
Muhammed Esad ŞEKER <76184668+uxino@users.noreply.github.com>
2022-09-07 06:19:26 +0300

    randomly creating route(not working)

--- Assets/Assets/Scripts/Maps/MapGeneratorOop.cs
+++ Assets/Assets/Scripts/Maps/MapGeneratorOop.cs
@@ -10,11 +10,11 @@ public class MapGeneratorOop : MonoBehaviour
 {
     public System.Random a = new System.Random();
     public List<int> uniqueRandomList = new List<int>();
+    public List<int> tempRandomList = new List<int>();
     public List<int> randomHorizontalAxisValues = new List<int>();
     public GameObject node,parent;
-    public List<Vector2> fullGrids;
     int row=10;
-    int q;
+    int q,random;
 
     private void Start()
     {
@@ -23,15 +23,65 @@ public class MapGeneratorOop : MonoBehaviour
         for (int i = 0; i < row; i++)
         {            
             q=Random.Range(2, 5);//how many nodes will be created
-
-            for (int j = 0; j < q; j++)
+            if (i == 0)
             {
-                uniqueRandomList.Add(NewNumber());
-                fullGrids.Add(new Vector2(i, uniqueRandomList[j]));//if(i!=0)Arkadaki konumlara bakarak oluştur fullgridse bak
-                GameObject tempNode = Instantiate(node,parentObject);
-                Grid.Move(tempNode, uniqueRandomList[j],i );
-                tempNode.name = (i+"x"+uniqueRandomList[j]);
+                for (int j = 0; j < q; j++)
+                {
+                    uniqueRandomList.Add(NewNumber());
+                    GameObject tempNode = Instantiate(node, parentObject);
+                    Grid.Move(tempNode, uniqueRandomList[j], i);
+                    tempNode.name = (i + "x" + uniqueRandomList[j]);
+                    tempRandomList = uniqueRandomList;
+
+                }
+                foreach (var p in tempRandomList)
+                { print(p); }
             }
+            else
+            {
+                print("sa");
+                foreach (var p in tempRandomList)
+                {print(p);}
+                //foreach (var p in tempRandomList)
+                //{
+                //    print("saydırıyorum");
+                //    int temp = (int)p;
+                //    random = Random.Range(0, 3);
+                //    print(random);
+                //    if (random==0&&temp!=0)//left
+                //    {
+                //        print("girdim");
+                //        uniqueRandomList.Add(NewNumber());
+                //        //fullGrids.Add(new Vector2(i, uniqueRandomList[j]));//if(i!=0)Arkadaki konumlara bakarak oluştur fullgridse bak
+                //        GameObject tempNode = Instantiate(node, parentObject);
+                //        Grid.Move(tempNode, uniqueRandomList[p-1], i);
+                //        tempNode.name = (i + "x" + uniqueRandomList[p-1]);
+                //        tempRandomList = uniqueRandomList;
+                //    }
+                //    else if (random == 1&&temp!=6)//right
+                //    {
+                //        print("girdim");
+                //        uniqueRandomList.Add(NewNumber());
+                //        //fullGrids.Add(new Vector2(i, uniqueRandomList[j]));//if(i!=0)Arkadaki konumlara bakarak oluştur fullgridse bak
+                //        GameObject tempNode = Instantiate(node, parentObject);
+                //        Grid.Move(tempNode, uniqueRandomList[p], i);
+                //        tempNode.name = (i + "x" + uniqueRandomList[p]);
+                //        tempRandomList = uniqueRandomList;
+                //    }
+                //    else//front
+                //    {
+                //        print("girdim");
+                //        uniqueRandomList.Add(NewNumber());
+                //        GameObject tempNode = Instantiate(node, parentObject);
+                //        Grid.Move(tempNode, uniqueRandomList[p + 1], i);
+                //        tempNode.name = (i + "x" + uniqueRandomList[p + 1]);
+                //        tempRandomList = uniqueRandomList;
+                //    }
+
+
+                //}
+            }
+            
             uniqueRandomList.Clear();
 
             //List<int> horizontalPositions = uniqueRandomList;
