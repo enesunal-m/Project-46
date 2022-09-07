3d1a54d63073fe1df18ce6063d8a8324fbef9313
Muhammed Esad ŞEKER <76184668+uxino@users.noreply.github.com>
2022-09-07 03:25:39 +0300

    Random Generated Nodes Ver:1.0

--- Assets/Assets/Scripts/Maps/Grid.cs
+++ Assets/Assets/Scripts/Maps/Grid.cs
@@ -2,12 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grid : MonoBehaviour
+public static class Grid
 {
-    //int[] public static canvasMeasurer()
-    //{
-        //int[] x =new int[2];
-        //x =[3, 4];
-        //return x;
-    //}
+    public static void Move(GameObject node,int desiredRow,int desiredColumn)
+    {
+        //Later will be changed with variables
+        int maxWidth = 800;
+        int maxHeigth = 1200;
+        int row = 10;
+        int column = 7;
+
+        //distances between Grid tiles
+        int horizontalDistance = maxWidth / column;
+        int verticalDistance = maxHeigth / row;
+        Vector2 gridIndexes;
+
+        float x = 100+desiredRow*horizontalDistance;
+        float y = 250+desiredColumn * verticalDistance;
+        gridIndexes = new Vector2(x, y);
+        node.GetComponent<Transform>().position = gridIndexes;
+    }
+
+
+
 }
--- Assets/Assets/Scripts/Maps/MapGeneratorOop.cs
+++ Assets/Assets/Scripts/Maps/MapGeneratorOop.cs
@@ -1,164 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+using Random = UnityEngine.Random;
 
 public class MapGeneratorOop : MonoBehaviour
 {
-    int row = 10;
-    int column = 7;
-    [SerializeField] GameObject node, mapParent, canvas;
-    Vector2[,] nodePositionArray;
-    List<MapNode> nodeRowColumnIndex;
+    public System.Random a = new System.Random();
+    public List<int> uniqueRandomList = new List<int>();
+    public List<int> randomHorizontalAxisValues = new List<int>();
+    public GameObject node,parent;
+    int row=10;
+    int q;
 
-
-    public static List<int> nodesPositions(int max, int nodesNum)
+    private void Start()
     {
-        List<int> randNums = new List<int>();
-        int a = 0;
-        while (randNums.Count < nodesNum)
-        {
-            a = Random.Range(0, max);
-            if (!randNums.Contains(a))
+        Transform parentObject = parent.GetComponent<Transform>();
+        int x = 0;
+        for (int i = 0; i < row; i++)
+        {            
+            q=Random.Range(2, 4);//how many nodes will be created
+
+            for (int j = 0; j < q; j++)
             {
-                randNums.Add(a);
+                uniqueRandomList.Add(NewNumber());
+                GameObject tempNode = Instantiate(node,parentObject);
+                Grid.Move(tempNode, uniqueRandomList[j],i );
+                tempNode.name = (i+" "+j);
             }
-        }
-        return randNums;
-    }
+            uniqueRandomList.Clear();
 
-    public static List<int> nodesNumberInRow()
+            //List<int> horizontalPositions = uniqueRandomList;
+
+        }
+        print(x);
+
+
+    }
+    
+    private int NewNumber()
     {
-        List<int> nodesNum = new List<int>();
-        int a = 0;
-        for (int i = 0; i < 10; i++)
+        int myNumber = Random.Range(0, 7);
+        while (uniqueRandomList.Contains(myNumber))
         {
-            a = Random.Range(0, 4);
-            //To maintain good destrubition of nodes number in row (2,3,4 -> 20%,60%,20%)
-            switch (a)
-            {
-                case 0:
-                    nodesNum.Add(2);
-                    break;
-                case 4:
-                    nodesNum.Add(4);
-                    break;
-                default:
-                    nodesNum.Add(3);
-                    break;
-            }
+            myNumber = Random.Range(0, 7);
         }
-        return nodesNum;
-
+        return myNumber;
+        
     }
-    void Start()
-    {
-
-        int a = Random.Range(2, 4);
-        if(a == 2)
-        { 
-            MapNode nodes = new MapNode(node, new Vector2(900, 5));
-            nodeRowColumnIndex.Add(nodes.GetComponent<MapNode>());
-        }
-        else if (a == 3)
-        {
-            MapNode nodes = new MapNode(node, new Vector2(900, 5));
-            nodeRowColumnIndex.Add(nodes.GetComponent<MapNode>());
-        }
-        else
-        {
-            MapNode nodes = new MapNode(node, new Vector2(900, 5));
-            nodeRowColumnIndex.Add(nodes.GetComponent<MapNode>());
-        }
-        print(a + "geldi");
-
-        //Instantiate(nodes[i,j],)
-        //nodes[i, j] = Instantiate(node);
-        //nodes[i, j].transform.SetParent(mapParent.transform, false);
-        //nodes[i, j].name = "node" + i + "x" + j;
 
 
 
-    }
-    //NodePositionerAndPositionHolder(nodes);
 }
-
-
-
-    //void NodePositionerAndPositionHolder(GameObject[,] nodes)
-    //{
-        //Canvas Dimensions
-
-        //float portionalWidthOfMap = canvas.GetComponent<CanvasScaler>().referenceResolution.x / column;
-        //float portionalHeightOfMap = canvas.GetComponent<CanvasScaler>().referenceResolution.y / row;
-        //float nodeDiameter = node.GetComponent<RectTransform>().rect.width;
-        ////int skipNodeProductionCounter = 0;
-        //float k = Random.Range(-portionalWidthOfMap * 0.05f, portionalHeightOfMap * 0.05f);
-
-        //Canvas Dimensions
-
-        //static int newnumber(int r)
-        //{
-        //    int[] numbers = new int[r];
-        //    int a = random.range(0, r);
-        //    if (numbers.contains(a))
-        //    {
-        //        a = random.range(0, r);
-        //    }
-        //    return a;
-        //}
-        //List<int> nodeNumber = nodesNumberInRow();
-
-        //for (int i = 0; i < column; i++)
-        //{
-        //    int nodesNumber = Random.Range(2, 4);
-        //    List<int> randNums = nodesPositions(7, nodesNumber);
-        //    foreach (int j in randNums)
-        //    {
-        //        Vector2 temp = new Vector2(i * 1.5f * portionalWidthOfMap, 1.3f * j * portionalHeightOfMap);
-        //        nodes[i, j].GetComponent<RectTransform>().anchoredPosition = temp;
-        //        //odePositionArray[i, j] = temp;
-        //        Debug.Log(j);
-        //    }
-
-
-            //List<int> nodeNumber = nodesNumberInRow();
-            ////foreach (int node in nodeNumber)   
-
-            //// int nodesNumber = Random.Range(2, 4);
-            //foreach (int j in nodeNumber)
-            //{
-            //    List<int> randNums = nodesPositions(7, j);
-            //    foreach (int n in randNums)
-            //    {
-            //        Vector2 temp = new Vector2(i * 1.5f * portionalWidthOfMap, 1.3f * n * portionalHeightOfMap);
-            //        nodes[i, n].GetComponent<RectTransform>().anchoredPosition = temp;
-            //        //odePositionArray[i, j] = temp;
-            //        Debug.Log(n);
-            //    }
-
-            //}
-
-
-
-            //for (int j = 0; j < row; j++)
-            //{
-            //    if (Random.Range(0f, 10f) < 1f && skipNodeProductionCounter < 4)
-            //    {
-            //        i++;
-            //        skipNodeProductionCounter++;
-            //        //continue;
-            //    }
-            //    else
-            //    {
-            //        //screen sizes
-            //        Vector2 temp = new Vector2(i * 1.5f * portionalWidthOfMap, 1.3f * j * portionalHeightOfMap);
-            //        nodes[i, j].GetComponent<RectTransform>().anchoredPosition = temp;
-            //        //nodePositionArray[i, j] = temp;
-            //    }
-            //}
-
-        //}
-    //}
-//}
 
