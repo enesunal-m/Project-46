a16d02574f350c97578ce2a3d76516c512b1cfe5
mustafabukh <62837072+mustafabukh@users.noreply.github.com>
2022-09-08 20:30:22 +0300

    NewUI

--- Assets/Assets/Scripts/UIScrollRawImage.cs
+++ Assets/Assets/Scripts/UIScrollRawImage.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class UIScrollRawImage : MonoBehaviour
 {
     [SerializeField] float verticalSpeed;
+    [SerializeField] float horizontalSpeed;
     RawImage rawImage;
     // Start is called before the first frame update
     void Start()
@@ -18,11 +19,14 @@ public class UIScrollRawImage : MonoBehaviour
     {
         Rect currentUv = rawImage.uvRect;
         currentUv.y-=Time.deltaTime*verticalSpeed;
+        currentUv.x-=Time.deltaTime*horizontalSpeed;
       
-        if (currentUv.y <= -1f || currentUv.y >= 1f)
-        {
+        if (currentUv.y <= -1f || currentUv.y >= 1f)        
             currentUv.y = 0f;
-        }
+        
+        if (currentUv. x<= -1f || currentUv.x >= 1f)        
+            currentUv.x = 0f;
+        
         rawImage.uvRect = currentUv;
     }
 }
