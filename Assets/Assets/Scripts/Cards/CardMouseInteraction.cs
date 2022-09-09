831b32bf8e83c1996cee2b31ba50c3c195a91a55
bince25 <incebaran25@gmail.com>
2022-09-09 19:28:53 +0300

    Liarmeter In Progress

--- Assets/Assets/Scripts/Cards/CardMouseInteraction.cs
+++ Assets/Assets/Scripts/Cards/CardMouseInteraction.cs
@@ -43,7 +43,7 @@ public class CardMouseInteraction : MonoBehaviour, IPointerEnterHandler, IPointe
             {
                 highlightedCard = this.gameObject;
                 this.GetComponent<RectTransform>().localScale = new Vector3(1.2f, 1.2f, 1);
-                this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 80, this.transform.position.z);
+                this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 100, this.transform.position.z);
                 this.GetComponent<Canvas>().sortingOrder += 100;
             }
         }
@@ -64,7 +64,7 @@ public class CardMouseInteraction : MonoBehaviour, IPointerEnterHandler, IPointe
             {
                 highlightedCard = null;
                 this.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
-                this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - 80, this.transform.position.z);
+                this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - 100, this.transform.position.z);
                 this.GetComponent<Canvas>().sortingOrder -= 100;
             }
         }
