753f41e21fcd4b6bf6a2db48b56709c98c262c14
berketurker <112757322+berketurker@users.noreply.github.com>
2022-09-16 19:51:54 +0300

    Comment lines will be added

--- Assets/Assets/Scripts/LineController.cs
+++ Assets/Assets/Scripts/LineController.cs
@@ -10,50 +10,45 @@ public class LineController : MonoBehaviour
     public LineRenderer linerenderer;
     public List<Vector2> FingerPositions;
     public bool drawLine = true;
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
+    //Update is called once per frame
     void Update()
     {
-        //    if (drawLine)
-        //    {
-        //        currentLine = Instantiate(lineprefab, Vector3.zero, Quaternion.identity);
+        if (drawLine)
+        {
+            currentLine = Instantiate(lineprefab, Vector3.zero, Quaternion.identity);
 
-        //        drawLine = false;
-        //    }
+            drawLine = false;
+        }
+        CreateLine();
 
-        //    CreateLine();
-        //    /*if (Input.GetMouseButton(0))
-        //    {
-        //        Vector2 temFingerPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //        if (Vector2.Distance(temFingerPos, FingerPositions[FingerPositions.Count - 1]) > .1f)
-        //        {
+        /*if (Input.GetMouseButton(0))
+        {
+            Vector2 temFingerPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (Vector2.Distance(temFingerPos, FingerPositions[FingerPositions.Count - 1]) > .1f)
+            {
 
-        //            UpdateLine(temFingerPos);
-        //        }
-        //    }*/
-        //}
+                UpdateLine(temFingerPos);
+            }
+        }*/
+    }
 
 
-        //void CreateLine()
-        //{       
-        //    linerenderer = currentLine.GetComponent<LineRenderer>();
-        //    FingerPositions.Clear();
-        //    FingerPositions.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-        //    FingerPositions.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-        //    linerenderer.SetPosition(0, Vector3.zero);
-        //    linerenderer.SetPosition(1, FingerPositions[1]);
-        //}
+    void CreateLine()
+    {
+        linerenderer = currentLine.GetComponent<LineRenderer>();
+        FingerPositions.Clear();
+        FingerPositions.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        FingerPositions.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        linerenderer.SetPosition(0, Vector3.zero);
+        linerenderer.SetPosition(1, FingerPositions[1]);
+    }
 
-        //void UpdateLine(Vector2 newFingerPos)
-        //{
-        //    FingerPositions.Add(newFingerPos);
-        //    linerenderer.positionCount++;
-        //    linerenderer.SetPosition(linerenderer.positionCount - 1, newFingerPos);
-        //}
+    void UpdateLine(Vector2 newFingerPos)
+    {
+        FingerPositions.Add(newFingerPos);
+        linerenderer.positionCount++;
+        linerenderer.SetPosition(linerenderer.positionCount - 1, newFingerPos);
     }
 }
 
